06c5dfd3d686c1408bb179541420d4f0e32e7400
rakshitmehra <rakshitmehra88@gmail.com>
2024-02-29 02:30:15 +0530

    URL

--- soil_prediction.py
+++ soil_prediction.py
@@ -128,7 +128,7 @@ if (selected == 'Soil Prediction'):
                 )
             
             try:
-                response = requests.post('https://flaskappsoil01.azurewebsites.net/', json=input_features)
+                response = requests.post('http://localhost:5000', json=input_features)
                 if response.status_code == 200:
                     result = response.json()
                     soil = result.get("prediction")
